0ebaca572c70c6e0b0f3520bd4429d29dec5987f
Brian Yang <yangbri1@msu.edu>
2025-01-17 21:17:32 +0000

    Switched .toString() method to String.valueOf() method to lexographically compare two String's --- String.valueOf() does .toString() & returns "null" if instance is null. Meanwhile, .toString() will just throw a NullPointerException if String DNE. --- Additionally could have used Arrays.compare(a, b) to directly compare String's in a to b lexographically.

--- src/main/java/CharComparison.java
+++ src/main/java/CharComparison.java
@@ -11,19 +11,33 @@ public class CharComparison {
      * @return -1 if A is less than B, 1 if A is greater than B, and 0 if the two arrays are identical.
      */
     public int compare(char[] a, char[] b){
-        String aStr = a.toString();
-        String bStr = b.toString();
+        // .toString() also convert String to String type BUT will throw a NullPointerException if instance is null
+        // String aStr = a.toString();
+        // String bStr = b.toString();
+        /* Use valueOf() over .toString()
+        ** String.valueOf() method will convert String .toString() & will return "null" if instance is null */
+        // https://stackoverflow.com/questions/27465731/string-valueof-vs-object-tostring
+        String aStr = String.valueOf(a);
+        String bStr = String.valueOf(b);
+
+        // int lexOrder2 = java.util.Arrays.compare(a, b);  // Arrays.compare(a, b) method on char[] works too
         int lexOrder = aStr.compareTo(bStr);
+        // if aStr & bStr are lexographically equal ... i.e., char 'c' is before char 'd' --- c > d
         if(lexOrder == 0){
+            // indicate so
             return 0;
         }
-        else if(lexOrder < 0){
-            return -1;
-        }
-        else{
-            return 1;
-        }
+        /* else if aStr < bStr ... */
+        // else if(lexOrder < 0){
+        //     return (-1);
+        // }
+        /* else if aStr > bStr ... */
+        // else{
+        //     return 1;
+        // }
 
-        // return ((lexOrder < 0)? -1 : 1);
+        // ternary operation for else-if & else block above -- works too
+        return ((lexOrder < 0) ? (-1) : 1);
+
     }
 }
